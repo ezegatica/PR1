56312e4fb94c64ab3d7ef1455a40dac634785128
Eze Gatica <git@ezegatica.com>
2023-05-14 13:06:30 -0300

    TP4 entrega terminada!

---
+++ TestersPR1/src/ejercicio09/Main.java
@@ -0,0 +1,13 @@
+package ejercicio09;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] numero = new int[5];
+		int pepe = 1123123;
+		numero[0] = pepe;
+		pepe = 0;
+		System.out.println(numero[0]);
+	}
+
+}
